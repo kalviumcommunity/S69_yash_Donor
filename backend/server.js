7239702f5a5b111b076d45f909b42cbfb2eb0bfd
yashuwantjohn <yashuwant.john.s69@kalvium.community>
2025-03-27 17:48:44 +0530

    post api

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port: http://localhost:${PORT}`);
 });
